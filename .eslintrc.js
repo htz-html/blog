47a2ddcdddfa34f6bba6b9b7507fa574f2f5c6a3
htz-html <1006676722@qq.com>
2020-12-15 16:07:10 +0800

    修改 eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,11 +14,11 @@ module.exports = {
     ecmaVersion: 2020
   },
   rules: {
-    // indent: [2, 2, { SwitchCase: 1 }],  //强制一致的缩进风格
-    // eqeqeq: [2, "always"],   // 必须使用全等
-    // "no-undef": 0,  //不允许未声明的变量
-    // "no-unexpected-multiline": 2,  //避免多行表达式
-    // "no-unused-vars": 0,  //不允许有声明后未使用的变量或者参数
+    indent: [2, 2, { SwitchCase: 1 }],  //强制一致的缩进风格
+    eqeqeq: [2, "always"],   // 必须使用全等
+    "no-undef": 0,  //不允许未声明的变量
+    "no-unexpected-multiline": 2,  //避免多行表达式
+    "no-unused-vars": 0,  //不允许有声明后未使用的变量或者参数
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   },
