4e507c50225b51cd04ffeefc3ce86f181f89d140
htz-html <1006676722@qq.com>
2020-12-15 16:19:47 +0800

    创建四个页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -11,12 +11,19 @@ const routes: Array<RouteConfig> = [
     component: Home
   },
   {
+    path: '/resume',
+    name: 'Resume',
+    component: () => import('@/views/Resume.vue')
+  },
+  {
+    path: '/article',
+    name: 'Article',
+    component: () => import('@/views/Article.vue')
+  },
+  {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import('@/views/About.vue')
   }
 ]
 
---
+++ src/views/Resume.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    我是简历
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+}
+</script>
+<style lang="scss" scoped>
+</style>
