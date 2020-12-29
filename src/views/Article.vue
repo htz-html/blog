70d4f2eb0b89bf8a9f7d4c377ef49389e5784e7e
htz-html <1006676722@qq.com>
2020-12-29 15:40:02 +0800

    完成文章列表页分类

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -1,24 +1,33 @@
 <template>
   <Layout :isSearch="isSearch">
-    <div class="article-item" style="overflow:auto;height: 100vh;">
-    <ul
-      class="list"
-      v-infinite-scroll="load"
-      infinite-scroll-disabled="disabled">
-      <li v-for="item in list" :key="item.id" @click="goArticleDetail(item)">
-        <div class="top text">
-          <h2 class="ellipsis-2">{{item.title}}</h2>
-          <div class="describe ellipsis-2">{{item.describe}}</div>
-        </div>
-        <div class="bottom">
-          {{item.user}} · {{item.time}} · {{item.type}}
-        </div>
-      </li>
-    </ul>
-    <div class="loading">
-      <p v-if="loading" class="rotate"><i class="iconfont iconloading"></i></p>
-      <p v-if="noMore">没有更多了</p>
-    </div>
+    <div class="article">
+      <el-tabs v-model="activeName" @tab-click="handleClick">
+        <el-tab-pane 
+          v-for="item in typeList" 
+          :key="item+Math.random()" 
+          :label="item" :name="item">
+          <div  class="article-item" style="overflow:auto;height: 100vh;">
+            <ul
+              class="list"
+              v-infinite-scroll="load"
+              infinite-scroll-disabled="disabled">
+              <li v-for="item in firstList" :key="item.id" @click="goArticleDetail(item)">
+                <div class="top text">
+                  <h2 class="ellipsis-2">{{item.title}}</h2>
+                  <div class="describe ellipsis-2">{{item.describe}}</div>
+                </div>
+                <div class="bottom">
+                  {{item.user}} · {{item.time}} · {{item.type}}
+                </div>
+              </li>
+            </ul>
+            <div class="loading">
+              <p v-if="loading" class="rotate"><i class="iconfont iconloading"></i></p>
+              <p v-if="noMore">没有更多了</p>
+            </div>
+          </div>
+        </el-tab-pane>
+      </el-tabs>
   </div>
   </Layout>
 </template>
@@ -29,29 +38,35 @@ import { Component, Vue } from "vue-property-decorator";
   components:{},
 })
 export default class Home extends Vue {
-  count=12;
+  count=6;
   loading= false;
   list = [] as any [];
+  firstList=[] as any [];
   isSearch = this.$route.meta.isSearch;
+  allArticlesData = [];
+  typeList=[];
+  activeName='';
+
   get noMore(){
-    return this.count >= this.articlesData.length
+    return this.count >= this.list.length
   }
   get disabled(){
     return this.loading || this.noMore
   }
-  articlesData = []
   created() {
-    this.articlesData = this.$store.state.dataArticle;
-    this.list = this.articlesData.slice(0,this.count);
+    this.allArticlesData = this.$store.state.dataArticle;
+    this.getTypes()
+    this.filterTypeData(this.activeName)
+    this.firstList = this.list.slice(0,this.count);
   }
   load () {
     this.loading = true;
-    const moreData = this.articlesData.slice(this.count, this.count+2)
+    const moreData = this.list.slice(this.count, this.count+5)
     setTimeout(() => {
       moreData.forEach(item=>{
-        this.list.push(item)
+        this.firstList.push(item)
       })
-      this.count += 2
+      this.count += 5
       this.loading = false
     }, 2000)
   }
@@ -62,13 +77,52 @@ export default class Home extends Vue {
       this.$router.push({path: "pagedetail", query:{id: item.id}})
     }
   }
+  handleClick(tab: any) {
+    this.list = []
+    this.filterTypeData(tab.name)
+    this.firstList = this.list.slice(0,this.count);
+  }
+  getTypes(){
+    const typeAll: any = []
+    this.allArticlesData.forEach((item: any)=>{
+      typeAll.push(item.type)
+    })
+    this.typeList = Array.from(new Set(typeAll));
+    this.activeName = this.typeList[0]
+  }
+  filterTypeData(tab: string){
+    this.list = this.allArticlesData.filter((item: any) => (item.type === tab))
+  }
 }
 </script>
-
 <style lang="scss">
 @keyframes rotate{
   0%{transform: rotate(0deg)}
   100%{transform: rotate(360deg)}
+}
+.article{
+  .el-tabs__header{
+    margin: 0;
+    background: #32343d;
+    padding: 0 20px;
+    border-radius: 5px 5px 0 0;
+  }
+  .el-tabs__nav-wrap::after{
+    width: 0;
+  }
+  .el-tabs__item{
+    font-size: 18px;
+    height: 60px;
+    line-height: 60px;
+    color: #aaa;
+    &.is-active{
+      color:#47CF74;
+    }
+  }
+  .el-tabs__active-bar{
+    background-color: #47CF74;
+  }
+  
 }
 .article-item{
   .loading {
@@ -96,8 +150,10 @@ $gridGap: 30px;
 $lookBtn: 44px;
 $bottomHeight:44px;
 $textPadding:20px;
+.article{
+  margin-top: 50px;
+}
 .article-item{
-  margin-top: 120px;
   > ul {
     >li {
       background: #1F2026;
